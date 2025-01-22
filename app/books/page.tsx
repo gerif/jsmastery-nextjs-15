dfe544f89d12d3ad5417be3f652c72b1c076b11a
Erik <erdauletbayangali@gmail.com>
2025-01-22 23:11:04 +0600

    Set up light theme

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -5,10 +5,13 @@ const Page = async () => {
   const books = await response.json();
   return (
     <div>
+      <h1 className={"font-space-grotesk text-7xl font-extrabold"}>
+        The name of the books WWWW yyyy YYYY
+      </h1>
       {books.map((book) => (
         <div key={book.id}>
-          <h1>{book.name} + my book on my page</h1>
-          <h3>{book.id}</h3>
+          <h1 className={"h2-bold"}>{book.name} + my book on my page</h1>
+          <h3 className={"font-space-grotesk"}>{book.id}</h3>
         </div>
       ))}
     </div>
--- app/products/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const First = () => {
-  return <div>First Page 1</div>;
-};
-
-export default First;
