ffc4baf2ecc3983de330687eacb7ecc32bfae649
Erik <erdauletbayangali@gmail.com>
2025-03-20 00:19:44 +0600

    Mobile Nav link

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,9 +1,18 @@
+"use client";
 import React from "react";
+import AuthForm from "@/components/forms/AuthForm";
+import { SignInSchema } from "@/lib/validations";
 
 const Page = () => {
   return (
     <div>
       <h1>Sign In Page</h1>
+      <AuthForm
+        formType={"SIGN_IN"}
+        schema={SignInSchema}
+        defaultValues={{ email: "", password: "" }}
+        onSubmit={(data) => Promise.resolve({ success: true, data })}
+      />
     </div>
   );
 };
